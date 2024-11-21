5d073f16dc9aa2f3ad2ca7b03d7aacad9d56a42d
p0keyy <petertran241@gmail.com>
2024-11-20 23:47:56 -0800

    merging with mutex 2

--- esp32_webserv/.pio/build/esp32cam/libffe/ESP32QRCodeReader/quirc/decode.c.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32cam/libffe/ESP32QRCodeReader/quirc/decode.c.o: \
- .pio/libdeps/esp32cam/ESP32QRCodeReader/src/quirc/decode.c \
- .pio/libdeps/esp32cam/ESP32QRCodeReader/src/quirc/quirc_internal.h \
- .pio/libdeps/esp32cam/ESP32QRCodeReader/src/quirc/quirc.h
